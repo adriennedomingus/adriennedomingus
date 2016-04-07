133fa152ce7982f42128e35938d5950e392329da
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-04-07 15:47:27 -0600

    User can view individual blog

---
+++ spec/integration/visitor/visitor_views_blog_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.feature "visitor views specific blog" do
+  scenario "visitor views blog" do
+    blog1 = Blog.create(title: "First blog title", content: "some content", date: Date.new(2016, 03, 17))
+    blog2 = Blog.create(title: "Second blog title", content: "some more content", date: Date.new(2016, 03, 24))
+
+    visit blogs_path
+    click_on blog1.title
+    expect(page).to have_content(blog1.title)
+    expect(page).to have_content("March 17, 2016")
+    expect(page).to have_content(blog1.content)
+  end
+end
