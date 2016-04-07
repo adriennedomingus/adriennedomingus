8a641f071983f6df30d2a1f66916cd31e302b2fe
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-04-07 14:07:28 -0600

    User can log out

---
+++ spec/integration/user_logs_out_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.feature "user logs in" do
+  scenario "user logs in" do
+    user = User.create(username: "adrienne", password: "password", email: "adrienne.domingus@gmail.com")
+
+    visit login_path
+
+    fill_in :session_username, with: "adrienne"
+    fill_in :session_password, with: "password"
+    click_on "Log In"
+
+    click_on "Log Out"
+
+    refute page.has_content?("Log Out")
+  end
+end
