1b1c67ae64acace25839e01b529583fbb21df677
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-04-08 07:31:02 -0600

    User can create a blog

--- app/controllers/user/blogs_controller.rb
+++ app/controllers/user/blogs_controller.rb
@@ -1,4 +1,20 @@
 class User::BlogsController < User::BaseController
   def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blog_path(@blog)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :date, :content)
   end
 end
--- spec/integration/user/user_writes_a_blog_spec.rb
+++ spec/integration/user/user_writes_a_blog_spec.rb
@@ -2,6 +2,16 @@ RSpec.feature "user writes a blog" do
   scenario "logged in user writes a blog" do
     user = User.create(username: "adrienne", password: "password", email: "adrienne.domingus@gmail.com")
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit new_user_blog_path
+
+    fill_in :blog_title, with: "New Blog Title"
+    fill_in :blog_date, with: "2016/04/17"
+    fill_in :blog_content, with: "content content content content content content content content content content"
+    click_on "Create Blog"
+    expect(page).to have_content "New Blog Title"
+    expect(page).to have_content "April 17, 2016"
+    expect(page).to have_content "content content content content content content content content content content"
   end
 
   scenario "visitor cannot write a blog" do
